d840223c3635c86758340ffcad9839bf8d9e6f92
nimamirvahabi <mirvahabinima@gmail.com>
2022-06-13 16:44:29 +0430

    main_code_body

--- chat_app_main_code.py
+++ chat_app_main_code.py
@@ -1,1 +1,30 @@
 # github address: "https://github.com/nimamirvahabi/chat-app"
+
+def pos(x,y):
+    # curser goto the x,y position
+
+    return '\x1b[' + str(y) + ';' + str(x) + 'H'
+
+def write_file(text):
+    # write text to file
+    with open('./readme.txt', 'w') as f:
+            f.write(text)
+            f.close()
+def make_file():
+    # make file
+    with open('./readme.txt',"a") as f:
+        f.close()
+
+def read_file():
+    # read file
+    with open('./readme.txt',"r") as f:
+        text = f.readlines()
+        f.close()
+
+    return text
+
+#main_loop
+
+while True:
+
+    exit(0)
