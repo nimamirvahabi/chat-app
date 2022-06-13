e72bb02b8bdc7292743e4dccec52c317c2e9a0af
nimamirvahabi <mirvahabinima@gmail.com>
2022-06-13 17:20:15 +0430

    userpass setuped

--- chat_app_main_code.py
+++ chat_app_main_code.py
@@ -15,6 +15,21 @@ logo = """
             ( ( __))                                                                                 
         """
 
+# strat page 
+
+start_page = """ _____________________________________________________  \n
+                |                   log_in or sign_up                 | \n
+                | --------------------------------------------------- | \n
+                |                                                     | \n
+                |       sign_up                     log_in            | \n
+                |                                                     | \n
+                |       close                       introduce         | \n
+                |                                                     | \n
+                | --------------------------------------------------- | \n
+                |    please write your choose ->                      | \n
+                |_____________________________________________________| \n"""
+
+
 def pos(x,y):
     # curser goto the x,y position
 
@@ -32,11 +47,14 @@ def make_file():
 
 def read_file():
     # read file
-    with open('./userpass',"r") as f:
+    with open('./userpass.txt',"r") as f:
         text = f.readlines()
         f.close()
 
     return text
+
+def txt_prosses(text,by = ','):
+    return (text.split(by))
 
 def print_commend(commend,x = 2):
     #print logo that it get and show it for x sec
@@ -45,10 +63,37 @@ def print_commend(commend,x = 2):
     time.sleep(x)
     os.system("cls")
 
+def print_page(page):
+    #print page that it get and show it
+
+    print(page)
+
+userpass = read_file()
+
+def user_pass_prosses():
+    # user pass setup
+
+    global userpass
+    for i in range(len(read_file())):
+        userpass[i] = txt_prosses(read_file()[i])
+
+
+
+
+
+
+
+
+
+
+
+
+user_pass_prosses()
 
 #main_loop
 
 while True:
 
     print_commend(logo)
+    print(userpass)
     exit(0)
